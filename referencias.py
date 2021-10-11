2d261ce1fa4daffe15d3b49023fed820cb503d37
Marcos Vinicius <63943591+manteguinha@users.noreply.github.com>
2021-10-11 16:04:29 -0300

    Update 0.5a

--- referencias.py
+++ referencias.py
@@ -1,4 +1,3 @@
-from tkinter import font
 from tkinter.font import BOLD
 import PySimpleGUI as sg
 from datetime import date
@@ -19,8 +18,8 @@ class TelaPython:
             [sg.Text('Nome do site', size=(18,0)), sg.Input(size=(30,0), key='nome_site')],
             [sg.Text('Ano da postagem', size=(18,0)), sg.Input(size=(30,0), key='ano')],
             [sg.Text('Link', size=(18,0)), sg.Input(size=(30,0), key='link')],
-            [sg.Button('Criar referência',  font=("Arial", 10, BOLD)), sg.Button('Limpar tela', 
-                font=("Arial", 10, BOLD)),sg.Button('Fechar', 
+            [sg.Button('Criar referência',  font=("Arial", 10, BOLD)),  
+            sg.Button('Limpar tela',  font=("Arial", 10, BOLD)),sg.Button('Fechar', 
                 font=("Arial", 10, BOLD)), sg.Button('Versão',  font=("Arial", 10, BOLD),
                     button_color=('Black', 'Light Gray'))],   
             [sg.Output(size=(50,10), key='_OUTPUT_')]
@@ -30,14 +29,16 @@ class TelaPython:
     def Iniciar(self):
         while True:
             self.button, self.values = self.janela.Read()
+            if self.button == sg.WINDOW_CLOSED:
+                break
             if self.button == 'Fechar':
                 break
             if self.button == 'Versão':
-                sg.popup('Versão: 0.03a', '\nData da modificação: 26-07-2021', 
-                                    '\nMudanças: \nImplementação do botão Limpar tela. \nCorreção de bugs.', 
+                sg.popup('Versão: 0.05a', '\nData da modificação: 11-10-2021', 
+                                    '\nMudanças: \nLimpa os campos. \nCorreção de bugs.', 
                                     '\nCreated by: @manteguinha_mantega',
                      font=("Arial", 10, BOLD))
-            else:
+            if self.button == 'Criar referência':
                 nome_autor = self.values['autor']
                 nomeAutor = nome_autor.capitalize()
                 sobrenome_autor = self.values['sobrenome']
@@ -48,7 +49,21 @@ class TelaPython:
                 link_site = self.values['link']
                 print(f'{" ".join(sobrenomeAutor.split())}, {" ".join(nomeAutor.split())}. {" ".join(titulo_materia.split())}. {" ".join(site.split())}, {" ".join(ano_post.split())}. Disponível em: <{link_site.replace(" ", "")}>. Acesso em: {data_em_texto}.')
                 print(f'\n')
+                self.janela.find_element('autor').Update('')
+                self.janela.find_element('sobrenome').Update('')
+                self.janela.find_element('titulo').Update('')
+                self.janela.find_element('nome_site').Update('')
+                self.janela.find_element('ano').Update('')
+                self.janela.find_element('link').Update('')
+
             if self.button == 'Limpar tela':
                 self.janela['_OUTPUT_'].update(value='')
+                self.janela.find_element('autor').Update('')
+                self.janela.find_element('sobrenome').Update('')
+                self.janela.find_element('titulo').Update('')
+                self.janela.find_element('nome_site').Update('')
+                self.janela.find_element('ano').Update('')
+                self.janela.find_element('link').Update('')
+
 tela = TelaPython()
 tela.Iniciar()
